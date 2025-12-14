0742b028344b8762718254608bf3be42ce49759a
kensano-in <shinichiro.in@gmail.com>
2025-12-15 01:00:39 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,3 +1,20 @@
-function goChat() {
+async function goChat() {
+  const inputs = document.querySelectorAll("input");
+  const username = inputs[0].value;
+  const password = inputs[1].value;
+
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  if (!res.ok) {
+    alert("Access denied");
+    return;
+  }
+
+  const data = await res.json();
+  localStorage.setItem("kensano_token", data.token);
   window.location.href = "chat.html";
 }
